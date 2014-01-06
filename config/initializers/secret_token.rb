abf208220b18f6768fe26dd7b31d19be4d9e3c26
Alex Egan <aegan@andrew.cmu.edu>
2014-01-06 00:24:36 -0500

    initial commit. rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KarateRails4::Application.config.secret_key_base = 'e3244b84d725025a8240b9ca8e432412c61e573bff078d251fa97f339aaf96f562e8c07a2053c25b751d4f63a8f7643fa84f8d174d9d251c1d7871b09edaf48d'
