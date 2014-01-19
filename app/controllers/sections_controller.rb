8800af2d29e50e9009bf68e54a87bac7f086c7bf
Alex Egan <aegan@andrew.cmu.edu>
2014-01-19 15:09:34 -0500

    sections show registered students

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -10,6 +10,7 @@ class SectionsController < ApplicationController
   # GET /sections/1
   # GET /sections/1.json
   def show
+    @registrations = Registration.for_section(@section.id).by_student
   end
 
   # GET /sections/new
