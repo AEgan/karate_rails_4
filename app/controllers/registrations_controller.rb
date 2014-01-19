aec1c897aa0fde8131740653d603cf09c8930680
Alex Egan <aegan@andrew.cmu.edu>
2014-01-19 13:57:17 -0500

    base registration form set up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,6 +15,10 @@ class RegistrationsController < ApplicationController
   # GET /registrations/new
   def new
     @registration = Registration.new
+    @section_options = Array.new
+    Section.all.each do |sect|
+      @section_options << ["#{sect.event.name}, #{rank_name(sect.min_rank)} - #{rank_name(sect.max_rank)}, #{sect.min_age} years - #{sect.max_age} years", sect.id]
+    end
   end
 
   # GET /registrations/1/edit
@@ -25,7 +29,7 @@ class RegistrationsController < ApplicationController
   # POST /registrations.json
   def create
     @registration = Registration.new(registration_params)
-
+    @registration.date = Date.today
     respond_to do |format|
       if @registration.save
         format.html { redirect_to @registration, notice: 'Registration was successfully created.' }
@@ -70,5 +74,40 @@ class RegistrationsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def registration_params
       params.require(:registration).permit(:section_id, :student_id, :date)
+    end
+
+    # gets rank name for a rank
+    def rank_name(rank)
+      if rank == 1
+        "Tenth Gup"
+      elsif rank == 2
+        "Ninth Gup"
+      elsif rank == 3
+        "Eighth Gup"
+      elsif rank == 4
+        "Seventh Gup"
+      elsif rank == 5
+        "Sixth Gup"
+      elsif rank == 6
+        "Fifth Gup"
+      elsif rank == 7
+        "Fourth Gup"
+      elsif rank == 8
+        "Third Gup"
+      elsif rank == 9
+        "Second Gup"
+      elsif rank == 10
+        "First Gup"
+      elsif rank == 11
+        "First Dan"
+      elsif rank == 12
+        "Second Dan"
+      elsif rank == 13
+        "Third Dan"
+      elsif rank == 14
+        "Fourth Dan"
+      else
+        "Senior Master"
+      end
     end
 end
