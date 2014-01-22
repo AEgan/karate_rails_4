a37cf50927c331f078de53e0090bb636f8f3b012
Alex Egan <aegan@andrew.cmu.edu>
2014-01-21 21:36:15 -0500

    basic dojo model code in place

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -1,2 +1,25 @@
 class Dojo < ActiveRecord::Base
+	# relationships
+	# will be added when dojo_students is created
+
+	# Lists
+  	STATES_LIST = [['Alabama', 'AL'],['Alaska', 'AK'],['Arizona', 'AZ'],['Arkansas', 'AR'],['California', 'CA'],['Colorado', 'CO'],['Connectict', 'CT'],['Delaware', 'DE'],['District of Columbia ', 'DC'],['Florida', 'FL'],['Georgia', 'GA'],['Hawaii', 'HI'],['Idaho', 'ID'],['Illinois', 'IL'],['Indiana', 'IN'],['Iowa', 'IA'],['Kansas', 'KS'],['Kentucky', 'KY'],['Louisiana', 'LA'],['Maine', 'ME'],['Maryland', 'MD'],['Massachusetts', 'MA'],['Michigan', 'MI'],['Minnesota', 'MN'],['Mississippi', 'MS'],['Missouri', 'MO'],['Montana', 'MT'],['Nebraska', 'NE'],['Nevada', 'NV'],['New Hampshire', 'NH'],['New Jersey', 'NJ'],['New Mexico', 'NM'],['New York', 'NY'],['North Carolina','NC'],['North Dakota', 'ND'],['Ohio', 'OH'],['Oklahoma', 'OK'],['Oregon', 'OR'],['Pennsylvania', 'PA'],['Rhode Island', 'RI'],['South Carolina', 'SC'],['South Dakota', 'SD'],['Tennessee', 'TN'],['Texas', 'TX'],['Utah', 'UT'],['Vermont', 'VT'],['Virginia', 'VA'],['Washington', 'WA'],['West Virginia', 'WV'],['Wisconsin ', 'WI'],['Wyoming', 'WY']]
+
+	# validations
+	validates_presence_of :name, :street, :city
+	validates_uniqueness_of :name, case_sensitive: false
+	validates_inclusion_of :state, in: STATES_LIST.map { |k, v| v }, message: "is not recognized in the system"
+	validates_format_of :zip, with: /^\d{5}$/, message: "should be five digits long"
+	validates_inclusion_of :active, in: [true, false]
+
+	# scopes
+	scope :alphabetical, -> { order('name') }
+	scope :active, -> { where(active: true) }
+	scope :inactive, -> { where(active: false) }
+
+	# methods
+	# def current_students
+	#	will be included when relationships are added
+	# end
+	
 end
