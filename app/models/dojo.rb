13d6fd9025b12ab8152eac8cee0b47732f82b742
Alex Egan <aegan@andrew.cmu.edu>
2014-01-22 21:39:09 -0500

    Non context dojo tests in. Fixes regex for zip code

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -9,7 +9,7 @@ class Dojo < ActiveRecord::Base
 	validates_presence_of :name, :street, :city
 	validates_uniqueness_of :name, case_sensitive: false
 	validates_inclusion_of :state, in: STATES_LIST.map { |k, v| v }, message: "is not recognized in the system"
-	validates_format_of :zip, with: /^\d{5}$/, message: "should be five digits long"
+	validates_format_of :zip, with: /\A\d{5}\z/, message: "should be five digits long"
 	validates_inclusion_of :active, in: [true, false]
 
 	# scopes
--- test/models/dojo_test.rb
+++ test/models/dojo_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class DojoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  # relationships
+  # will be added when dojo_students is added
+
+  # validations
+  # presence
+  should validate_presence_of :name
+  should validate_presence_of :street
+  should validate_presence_of :city
+  # uniquness
+  should validate_uniqueness_of(:name).case_insensitive
+
+  # shoulda matchers
+  # zip
+  should allow_value("15213").for(:zip)
+  should allow_value(15213).for(:zip)
+  should_not allow_value("bad").for(:zip)
+  should_not allow_value("1512").for(:zip)
+  should_not allow_value("152134").for(:zip)
+  should_not allow_value("15213-0983").for(:zip)
+  # tests for state
+  should allow_value("OH").for(:state)
+  should allow_value("PA").for(:state)
+  should allow_value("WV").for(:state)
+  should_not allow_value("bad").for(:state)
+  should allow_value("NY").for(:state)
+  should_not allow_value(10).for(:state)
+  should allow_value("CA").for(:state)
+  # active
+  should allow_value(true).for(:active)
+  should allow_value(false).for(:active)
+  should_not allow_value(nil).for(:active)
+
+
+
+
 end
